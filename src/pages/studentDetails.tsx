e952d9d4584df8dcef6e80af38d64c5e682cc5ea
Sebastian Cerviño <cervisebas@gmail.com>
2022-03-27 15:49:05 -0300

    27/03/2022 15:49hs

---
+++ src/pages/studentDetails.tsx
@@ -0,0 +1,15 @@
+import { IonPage } from "@ionic/react";
+import { Component, ReactNode } from "react";
+
+type IProps = {};
+type IState = {};
+export default class StudentDetails extends Component<IProps, IState> {
+    constructor(props: IProps) {
+        super(props);
+    }
+    render(): ReactNode {
+        return(<IonPage>
+            
+        </IonPage>);
+    }
+}
