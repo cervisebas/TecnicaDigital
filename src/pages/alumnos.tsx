1266bf793f012f04092a40003c966a441a98393f
Sebastian Cerviño <cervisebas@gmail.com>
2022-04-06 09:11:17 -0300

    06/04/2022 09:11hs

--- src/pages/alumnos.tsx
+++ src/pages/alumnos.tsx
@@ -1,7 +1,8 @@
-import { IonAccordion, IonAccordionGroup, IonButtons, IonContent, IonHeader, IonImg, IonItem, IonLabel, IonList, IonMenuButton, IonPage, IonRefresher, IonRefresherContent, IonThumbnail, IonTitle, IonToolbar, RefresherEventDetail } from "@ionic/react";
+import { IonAccordion, IonAccordionGroup, IonButtons, IonContent, IonHeader, IonImg, IonItem, IonLabel, IonList, IonMenuButton, IonModal, IonPage, IonRefresher, IonRefresherContent, IonThumbnail, IonTitle, IonToolbar, RefresherEventDetail } from "@ionic/react";
 import { Component, ReactNode } from "react";
 import CustomScrollBar from "../components/CustomScrollBar";
 import './alumnos.scss';
+import StudentDetails from "./studentDetails";
 
 type IProps = {};
 type IState = {};
@@ -14,64 +15,65 @@ export default class ListAlumnos extends Component<IProps, IState> {
     }
     private array: string[] = [];
     render(): ReactNode {
-        return(<IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonButtons slot="start">
-                        <IonMenuButton color="primary" />
-                    </IonButtons>
-                    <IonTitle>Lista de alumnos</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent id="alumnos-content">
-              <IonRefresher slot="fixed" onIonRefresh={doRefresh}>
-                <IonRefresherContent></IonRefresherContent>
-              </IonRefresher>
-              <CustomScrollBar>
-                <IonAccordionGroup value={['colors', 'numbers']}>
-                  <IonAccordion value="colors">
-                    <IonItem slot="header">
-                      <IonLabel>3°1</IonLabel>
-                    </IonItem>
-                    <IonList slot="content">
-                      {this.array.map((value, index)=><IonItem lines={(index == this.array.length - 1)? 'none': undefined}>
-                        <IonThumbnail slot="start">
-                            <img style={{ borderRadius: '50%' }} src={'./assets/peppa.jpg'} />
-                        </IonThumbnail>
-                        <IonLabel>Alumno {String(index)}</IonLabel>
-                      </IonItem>)}
-                    </IonList>
-                  </IonAccordion>
-                  <IonAccordion value="shapes">
-                    <IonItem slot="header">
-                      <IonLabel>3°2°</IonLabel>
-                    </IonItem>
-                    <IonList slot="content">
-                    {this.array.map((value, index)=><IonItem lines={(index == this.array.length - 1)? 'none': undefined}>
-                        <IonThumbnail slot="start">
-                            <img style={{ borderRadius: '50%' }} src={'./assets/peppa.jpg'} />
-                        </IonThumbnail>
-                        <IonLabel>Alumno {String(index)}</IonLabel>
-                      </IonItem>)}
-                    </IonList>
-                  </IonAccordion>
-                  <IonAccordion value="numbers">
-                    <IonItem slot="header">
-                      <IonLabel>3°3°</IonLabel>
-                    </IonItem>
-                    <IonList slot="content">
-                    {this.array.map((value, index)=><IonItem lines={(index == this.array.length - 1)? 'none': undefined}>
-                        <IonThumbnail slot="start">
-                            <img style={{ borderRadius: '50%' }} src={'./assets/peppa.jpg'} />
-                        </IonThumbnail>
-                        <IonLabel>Alumno {String(index)}</IonLabel>
-                      </IonItem>)}
-                    </IonList>
-                  </IonAccordion>
-                </IonAccordionGroup>
-              </CustomScrollBar>
-            </IonContent>
-        </IonPage>);
+      return(<IonPage>
+          <IonHeader>
+              <IonToolbar>
+                  <IonButtons slot="start">
+                      <IonMenuButton color="primary" />
+                  </IonButtons>
+                  <IonTitle>Lista de alumnos</IonTitle>
+              </IonToolbar>
+          </IonHeader>
+          <IonContent id="alumnos-content">
+            <IonRefresher slot="fixed" onIonRefresh={doRefresh}>
+              <IonRefresherContent></IonRefresherContent>
+            </IonRefresher>
+            <CustomScrollBar>
+              <IonAccordionGroup value={['colors', 'numbers']}>
+                <IonAccordion value="colors">
+                  <IonItem slot="header">
+                    <IonLabel>3°1</IonLabel>
+                  </IonItem>
+                  <IonList slot="content">
+                    {this.array.map((value, index)=><IonItem button={true} lines={(index == this.array.length - 1)? 'none': undefined}>
+                      <IonThumbnail slot="start">
+                          <img style={{ borderRadius: '50%' }} src={'./assets/peppa.jpg'} />
+                      </IonThumbnail>
+                      <IonLabel>Alumno {String(index)}</IonLabel>
+                    </IonItem>)}
+                  </IonList>
+                </IonAccordion>
+                <IonAccordion value="shapes">
+                  <IonItem slot="header">
+                    <IonLabel>3°2°</IonLabel>
+                  </IonItem>
+                  <IonList slot="content">
+                  {this.array.map((value, index)=><IonItem lines={(index == this.array.length - 1)? 'none': undefined}>
+                      <IonThumbnail slot="start">
+                          <img style={{ borderRadius: '50%' }} src={'./assets/peppa.jpg'} />
+                      </IonThumbnail>
+                      <IonLabel>Alumno {String(index)}</IonLabel>
+                    </IonItem>)}
+                  </IonList>
+                </IonAccordion>
+                <IonAccordion value="numbers">
+                  <IonItem slot="header">
+                    <IonLabel>3°3°</IonLabel>
+                  </IonItem>
+                  <IonList slot="content">
+                  {this.array.map((value, index)=><IonItem lines={(index == this.array.length - 1)? 'none': undefined}>
+                      <IonThumbnail slot="start">
+                          <img style={{ borderRadius: '50%' }} src={'./assets/peppa.jpg'} />
+                      </IonThumbnail>
+                      <IonLabel>Alumno {String(index)}</IonLabel>
+                    </IonItem>)}
+                  </IonList>
+                </IonAccordion>
+              </IonAccordionGroup>
+            </CustomScrollBar>
+          </IonContent>
+          <StudentDetails show={true} />
+      </IonPage>);
     }
 }
 function doRefresh(event: CustomEvent<RefresherEventDetail>) {
--- src/pages/studentDetails.tsx
+++ src/pages/studentDetails.tsx
@@ -1,15 +1,46 @@
-import { IonPage } from "@ionic/react";
+import { IonAvatar, IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonImg, IonMenuButton, IonModal, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+import bowser from "bowser";
+import { arrowBackOutline } from "ionicons/icons";
 import { Component, ReactNode } from "react";
+import userDefault from '../assets/userDefault.svg';
 
-type IProps = {};
+type IProps = {
+    show: boolean;
+};
 type IState = {};
 export default class StudentDetails extends Component<IProps, IState> {
     constructor(props: IProps) {
         super(props);
     }
+    private modalRef: HTMLIonModalElement | null = null;
+    calculeModal() {
+        var browser = bowser.getParser(window.navigator.userAgent);
+        if (browser.getPlatform().type == 'desktop') {
+            var div: any = this.modalRef?.shadowRoot?.querySelector('div.modal-wrapper');
+            div.style.height = '100%';
+        }
+    }
     render(): ReactNode {
-        return(<IonPage>
-            
-        </IonPage>);
+        return(<IonModal ref={(ref)=>this.modalRef = ref} isOpen={this.props.show} backdropDismiss={true} onDidPresent={()=>this.calculeModal()}>
+            <IonPage>
+                <IonHeader>
+                    <IonToolbar>
+                        <IonButtons slot="start">
+                            <IonButton>
+                                <IonIcon slot="icon-only" color="primary" icon={arrowBackOutline} />
+                            </IonButton>
+                        </IonButtons>
+                        <IonTitle>Nombre del alumno</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonContent fullscreen>
+                    <div style={{ width: '100%', height: 256, margin: 12 }}>
+                        <IonAvatar>
+                            <IonImg src={userDefault} />
+                        </IonAvatar>
+                    </div>
+                </IonContent>
+            </IonPage>
+        </IonModal>);
     }
 }
